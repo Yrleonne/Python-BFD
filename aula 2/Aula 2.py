bbffa198b2c9082f211ba33c44fc3d85bd336edf
yrleonne <yrleonne@gmail.com>
2025-08-07 23:53:38 +0000

    print e variaveis

---
+++ aula 2/Aula 2.py
@@ -0,0 +1,36 @@
+
+'''
+soma +
+subtração -
+multiplicação *
+divisão / 
+divisão inteiro //
+modulo %
+expo **
+'''
+
+
+print('meu nome é yrleonne')
+print('tenho 36 anos')
+print('moro em recife')
+var1= 'meu nome é'
+var2= 'yrleonne'
+var3= 'moro em recife'
+print(var1,var2,var3)
+
+var4= '####'
+var5= '#  #'
+var6= '#  #'
+var7= '####'
+print (var4)
+print (var5)
+print (var6)
+print (var7)
+
+var10= 2
+var11= 2
+print(var10+var11)
+
+var13= 15
+var14= 3
+print(var13*var14)
